f16c2cf6839841604f15841798389ad60f54178f
Tony Messias <tonysm@hey.com>
2021-09-02 21:52:53 -0300

    Install RichTextLaravel

---
+++ config/rich-text-laravel.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Rich Text Model
+     |--------------------------------------------------------------------------
+     |
+     | When using the suggested database structure, all your Rich Text content will be
+     | stored in the same Database table. All interactions with that table happens
+     | using this Eloquent Model. You can override this if you really need to.
+     |
+     */
+    'model' => \Tonysm\RichTextLaravel\Models\RichText::class,
+];
