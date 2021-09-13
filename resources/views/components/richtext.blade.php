615e0e31e34372a56e2e1f3c815cc34b4b5fa86e
Tony Messias <tonysm@hey.com>
2021-09-13 00:00:42 -0300

    Small customization to the richtext field

--- resources/views/components/richtext.blade.php
+++ resources/views/components/richtext.blade.php
@@ -1,13 +1,20 @@
 @props(['id', 'value', 'name', 'disabled' => false])
 
-<input type="hidden" id="{{ $id }}_input" name="{{ $name }}" value="{{ $value?->toTrixHtml() }}" />
-<trix-editor
-    id="{{ $id }}"
-    input="{{ $id }}_input"
-    {{ $disabled ? 'disabled' : '' }}
-    {!! $attributes->merge(['class' => 'trix-content rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}
-    data-controller="trix-attachments"
-    data-action="
-        trix-attachment-add->trix-attachments#upload
-    "
->{{ $value }}</trix-editor>
+<div class="rounded-md group">
+    <input type="hidden" id="{{ $id }}_input" name="{{ $name }}" value="{{ $value?->toTrixHtml() }}" />
+    <div class="border border-gray-300 rounded-md focus-within:border-indigo-300 focus-within:ring focus-within:ring-indigo-200 focus-within:ring-opacity-50">
+        <trix-toolbar id="{{ $id }}_toolbar" class="sticky top-0 z-10 p-2 pb-0 bg-white border-b border-gray-300 rounded-t-md"></trix-toolbar>
+
+        <trix-editor
+            id="{{ $id }}"
+            input="{{ $id }}_input"
+            toolbar="{{ $id }}_toolbar"
+            {{ $disabled ? 'disabled' : '' }}
+            {!! $attributes->merge(['class' => 'trix-content p-2 focus-0 outline-none ring-0 rounded-0 border-0']) !!}
+            data-controller="trix-attachments"
+            data-action="
+                trix-attachment-add->trix-attachments#upload
+            "
+        >{{ $value }}</trix-editor>
+    </div>
+</div>
