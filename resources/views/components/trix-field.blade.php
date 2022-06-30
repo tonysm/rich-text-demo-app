4d55ad3866ca4c4a4ccfdcb65c5eaaf71a87cd26
Tony Messias <tonysm@hey.com>
2022-06-29 22:01:01 -0300

    Add Alpine component

--- resources/views/components/trix-field.blade.php
+++ resources/views/components/trix-field.blade.php
@@ -10,5 +10,24 @@
 <trix-editor
     id="{{ $id }}"
     input="{{ $id }}_input"
-    {{ $attributes->merge(['class' => 'trix-content']) }}
+    {{ $attributes->merge(['class' => 'trix-content rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) }}
+    x-data="{
+        upload(event) {
+            const data = new FormData();
+            data.append('attachment', event.attachment.file);
+
+            window.axios.post('/attachments', data, {
+                onUploadProgress(progressEvent) {
+                    event.attachment.setUploadProgress(
+                        progressEvent.loaded / progressEvent.total * 100
+                    );
+                },
+            }).then(({ data }) => {
+                event.attachment.setAttributes({
+                    url: data.image_url,
+                });
+            });
+        }
+    }"
+    x-on:trix-attachment-add="upload"
 ></trix-editor>
